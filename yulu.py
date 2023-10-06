87283e0bfea6b33a6855fa67812b18db8dfd2d72
MukeshMishraReloaded <131019954+MukeshMishraReloaded@users.noreply.github.com>
2023-10-07 00:42:18 +0530

    Update yulu.py

--- yulu.py
+++ yulu.py
@@ -43,7 +43,7 @@ df=total_count_by_weather_season[ (total_count_by_weather_season['season'] == s)
 #print the filtered dataframe
 st.dataframe(df)
 fig = plt.figure(figsize = (10, 5))
-plt.bar(['season', 'weather'], 'count')
+plt.bar(x=['season', 'weather'], y='count')
 plt.xlabel("Season and Weather")
 plt.ylabel("Total Users")
 plt.title("Total Casual and Registered Users")
