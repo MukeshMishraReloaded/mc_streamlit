b3cf560352abbbd9790ba67a6ce306eda877d2ce
MukeshMishraReloaded <131019954+MukeshMishraReloaded@users.noreply.github.com>
2023-10-06 21:57:16 +0530

    Add files via upload

--- yulu.py
+++ yulu.py
@@ -20,12 +20,6 @@ import matplotlib.pyplot as plt
 url = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/001/428/original/bike_sharing.csv?1642089089"
 df_yulu = pd.read_csv(url)
 
-# Plotting a histogram
-st.subheader('Histogram: count')
-plt.figure(figsize=(10,6))
-sns.histplot(data=df_yulu, x='count', kde=True)
-st.pyplot(plt.clf())
-
 # Display line chart
 
 st.write("""
@@ -37,16 +31,11 @@ season = st.selectbox(
     'Which season do you want to analyse?',
     ('1', '2', '3', '4'))
 
-st.dataframe(df_yulu['season'].unique())
-
-df=df_yulu
-
 st.write(f"""
     ###  Season {season}'s - renting of bikes data """)
 
-st.dataframe(df[df['season'] == 1][['casual', 'registered', 'count']])
-
-st.write(f"""
-    ### Season {season}'s - renting of bikes data""")
-
-st.line_chart(df[df['season'] == season]['count'])
+df=df_yulu[df_yulu['season'] == season][['casual', 'registered', 'count']]
+st.dataframe(df)
+# Plotting a histogram
+chart_data = df
+st.scatter_chart(chart_data)
