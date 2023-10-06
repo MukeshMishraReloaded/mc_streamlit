bcc1fe568b66c42433355f412ef0c20307a0c714
MukeshMishraReloaded <131019954+MukeshMishraReloaded@users.noreply.github.com>
2023-10-06 21:45:45 +0530

    Add files via upload

--- yulu.py
+++ yulu.py
@@ -19,38 +19,34 @@ import matplotlib.pyplot as plt
 
 url = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/001/428/original/bike_sharing.csv?1642089089"
 df_yulu = pd.read_csv(url)
-#df_yulu
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
 # Plotting a histogram
 st.subheader('Histogram: count')
 plt.figure(figsize=(10,6))
 sns.histplot(data=df_yulu, x='count', kde=True)
 st.pyplot(plt.clf())
 
-
 # Display line chart
-#st.line_chart(df.set_index('Date'))
-#st.write("""
+
+st.write("""
             # Yulu data analyzer """)
 
 ## get data for Yulu bikes
 
-#season = st.selectbox(
- #   'Which season do you want to analyse?',
- #   ('1', '2', '3', '4'))
+season = st.selectbox(
+    'Which season do you want to analyse?',
+    ('1', '2', '3', '4'))
 
-col1, col2 = st.columns(2)
+st.dataframe(df_yulu['season'].unique())
 
-#st.dataframe(df_yulu['season'].unique())
+df=df_yulu
 
-#df=df_yulu
-
-#st.write(f"""
+st.write(f"""
     ###  Season {season}'s - renting of bikes data """)
 
-#st.dataframe(df[df['season'] == 1][['casual', 'registered', 'count']])
+st.dataframe(df[df['season'] == 1][['casual', 'registered', 'count']])
 
-#st.write(f"""
+st.write(f"""
     ### Season {season}'s - renting of bikes data""")
 
-#st.line_chart(df[df['season'] == season]['count'])
+st.line_chart(df[df['season'] == season]['count'])
