9a838ce4a1181c55fe5cfd4ad215f51c21c726f8
MukeshMishraReloaded <131019954+MukeshMishraReloaded@users.noreply.github.com>
2024-02-11 11:11:49 +0530

    Update jamboree.py

--- jamboree.py
+++ jamboree.py
@@ -114,13 +114,13 @@ with col4:
             (1, 2, 3, 4, 5))
 
 # Using number_input to get a float value
-c = st.number_input("Input CGPA", min_value=0.0, max_value=10.0, value=0.0, step=0.1)
+c = st.number_input("CGPA: ", min_value=0.0, max_value=10.0, value=0.0, step=0.1)
 
 # Using number_input to get a float value
-g = st.number_input("Input GRE score", min_value=260.00, max_value=340.00, value=260.0, step=0.3)
+g = st.number_input("GRE score: ", min_value=260.00, max_value=340.00, value=260.0, step=0.3)
 
 # Using number_input to get a float value
-t = st.number_input("Input TOEFL score", min_value=0.0, max_value=120.0, value=0.0, step=0.3)
+t = st.number_input("TOEFL score: ", min_value=0.0, max_value=120.0, value=0.0, step=0.3)
 
 df_1=df.copy()
 df_1.drop(['Serial No.'], axis=1, inplace=True)
